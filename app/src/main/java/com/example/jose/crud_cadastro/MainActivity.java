8afd08602580bd80a442ea7e1de186bce8e05eb9
josé adilson <joseadilsson@gmail.com>
2016-12-14 21:22:38 -0200

    logout

--- app/src/main/java/com/example/jose/crud_cadastro/MainActivity.java
+++ app/src/main/java/com/example/jose/crud_cadastro/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.jose.crud_cadastro;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.example.jose.crud_cadastro.util.Mensagem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,16 +21,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
     }
 
-    public void onClickEntar(View view) {
-        Intent it = new Intent(MainActivity.this, CadUsuarioActivity.class);
-        startActivity(it);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_opcoes, menu);
+        return true;
     }
 
-    public void onClickMostrarLista(View view) {
-        Intent it = new Intent(MainActivity.this, ListaUsuariosActivity.class);
-        startActivity(it);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+
+        switch (item.getItemId()){
+            case R.id.itListaUsuario:
+                Intent itLista = new Intent(this, ListaUsuariosActivity.class);
+                startActivity(itLista);
+                break;
+
+            case R.id.itCadUsuario:
+                Intent itCadUsua = new Intent(this, CadUsuarioActivity.class);
+                startActivity(itCadUsua);
+                break;
+
+            case R.id.itSair:
+                Mensagem.MsgConfirm(this, "Sair", "Deseja realmente sair ?", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+                break;
+
+            case R.id.itLogout:
+                SharedPreferences preferences = getSharedPreferences("LoginActivityPreferences", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.clear().commit();
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
