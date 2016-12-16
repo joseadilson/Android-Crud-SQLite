9aa8230fd80027d12eeb92ac529cf971e2f1421b
josé adilson <joseadilsson@gmail.com>
2016-12-15 22:17:08 -0200

    adapter

---
+++ app/src/main/java/com/example/jose/crud_cadastro/adapter/TarefasAdapter.java
@@ -0,0 +1,31 @@
+package com.example.jose.crud_cadastro.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Jose on 15/12/2016.
+ */
+
+public class TarefasAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
