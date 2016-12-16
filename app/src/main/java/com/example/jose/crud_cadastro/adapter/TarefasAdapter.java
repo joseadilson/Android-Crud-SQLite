edccb7c557c76247f8984e809a56c27a02ab66d4
josé adilson <joseadilsson@gmail.com>
2016-12-16 12:46:03 -0200

    const

--- app/src/main/java/com/example/jose/crud_cadastro/adapter/TarefasAdapter.java
+++ app/src/main/java/com/example/jose/crud_cadastro/adapter/TarefasAdapter.java
@@ -1,14 +1,29 @@
 package com.example.jose.crud_cadastro.adapter;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+
+import com.example.jose.crud_cadastro.model.Tarefa;
+
+import java.util.List;
 
 /**
  * Created by Jose on 15/12/2016.
  */
 
 public class TarefasAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<Tarefa> lista;
+
+
+    public TarefasAdapter(Context ctx, List<Tarefa> tarefa){
+        this.context = ctx;
+        this.lista = tarefa;
+    }
+
     @Override
     public int getCount() {
         return 0;
