4500f92a2f09c09db2ffce0c66687db37254eb69
josé adilson <joseadilsson@gmail.com>
2016-12-16 13:02:42 -0200

    inflater

--- app/src/main/java/com/example/jose/crud_cadastro/adapter/TarefasAdapter.java
+++ app/src/main/java/com/example/jose/crud_cadastro/adapter/TarefasAdapter.java
@@ -1,10 +1,14 @@
 package com.example.jose.crud_cadastro.adapter;
 
+import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
+import com.example.jose.crud_cadastro.R;
 import com.example.jose.crud_cadastro.model.Tarefa;
 
 import java.util.List;
@@ -36,11 +40,21 @@ public class TarefasAdapter extends BaseAdapter {
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+    public View getView(int position, View view, ViewGroup parent) {
+        Tarefa tarefa = lista.get(position);
+
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater)context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.tarefas, null);
+        }
+
+        TextView lbNome = (TextView)view.findViewById(R.id.tarefaLista);
+        lbNome.setText(tarefa.getTarefa());
+
+        return view;
     }
 }
